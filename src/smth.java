d564918763198d9ac549b2db8e609358ffdca62b
Your Name <you@example.com>
2021-12-15 09:16:08 +0800

    V1.0

---
+++ src/smth.java
@@ -0,0 +1,47 @@
+public class smth {
+    public int a;
+    public double b;
+    public float c;
+
+    smth() {
+        a = 5;
+        b = 2.0;
+        c = 1f;
+    }
+
+    void urMOM() {
+
+    }
+
+    void shit() {
+
+    }
+
+    void compute(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            a += arr[i];
+        }
+    }
+
+    int[] computeArrTI(int[] arr, int digit) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (j == i)
+                    if (arr[i] + arr[j] == digit) {
+                        return new int[] { arr[i], arr[j] };
+                    }
+
+            }
+        }
+        return null;
+    }
+
+    int a() {
+        return a;
+    }
+
+    int b() {
+        return (int) b;
+    }
+
+}
